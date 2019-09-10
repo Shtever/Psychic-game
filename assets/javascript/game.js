74eaeb53e34e2e56b6d43b13ea4bce00818fb7a7
Stephen McMillen <Shtevetm@gmail.com>
2019-09-09 20:55:24 -0500

    functional, but not stylish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,46 @@
-
-
-
 var win = 0;
 var loss = 0;
 var guessLeft = 9;
-var letterGuess = []
-
-// ASCII characters to letters //
-// 97-122 = a-z
-
-// Pick random number 97-122 //
+var letterGuess = [];
 var min = 97;
 var max = 122;
 var random =
     Math.floor(Math.random() * (+max - +min)) + +min;
-
-function startOver (){
-    random = Math.floor(Math.random() * (+max - +min)) + +min;
-    return random;
-}
-
-// convert number to letter & console.log it//
 var letter = String.fromCharCode(random);
-console.log(letter);
+console.log("Solution: " + letter);
 
-// This function is run whenever the user presses a key.
+function reset() {
+    var min = 97;
+    var max = 122;
+    random = Math.floor(Math.random() * (+max - +min)) + +min;
+    letter = String.fromCharCode(random);
+    console.log("Solution: " + letter);
+    letterGuess = [];
+    document.getElementById("letterGuess").innerHTML = letterGuess;
+    guessLeft = 9;
+    document.getElementById("guessLeft").innerHTML = guessLeft;
+
+}
 document.onkeyup = function (event) {
-    // Determines which key was pressed.
     var userGuess = event.key;
-    console.log(userGuess);
-
-    if (guessLeft <= 1) {
+    if (guessLeft === 0) {
         loss++;
         document.getElementById("loss").innerHTML = loss;
         console.log("You lost!");
-        alert("You lost!");
-        guessLeft = 9;
-        letterGuess = [];
-        document.getElementById("guessLeft").innerHTML = 9;
-        console.log(letter);
-        console.log(userGuess);
+        alert("You Lost!")
+        reset();
     } else if (userGuess === letter) {
-            win++
-            alert("Nicely Done");
-            document.getElementById("win").innerHTML = win;
-            console.log("You Won!")
-            console.log(letter);
-        } else if (userGuess !== letter) {
-            guessLeft--;
-            letterGuess.push(event.key)
-            document.getElementById("guessLeft").innerHTML = guessLeft;
+        win++
+        alert("Nicely Done");
+        document.getElementById("win").innerHTML = win;
+        console.log("You Won!");
+        reset()
+
+    } else if (userGuess !== letter) {
+        guessLeft--;
+        document.getElementById("guessLeft").innerHTML = guessLeft;
+        console.log("Guess:" + userGuess);
+        letterGuess.push(userGuess);
+        document.getElementById("letterGuess").innerHTML = letterGuess;
     }
 }
